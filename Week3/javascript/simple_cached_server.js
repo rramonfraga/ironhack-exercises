2b86f8e0c1bbfd2c89853e73a3513ae4483cdd12
LluisArevalo <thorin119@gmail.com>
2015-11-12 14:41:22 +0100

    Add some variables to clarify the code

--- Week3/javascript/simple_cached_server.js
+++ Week3/javascript/simple_cached_server.js
@@ -4,17 +4,21 @@ var hash_array = {};
 
 var server = net.createServer(function(connection) {
     console.log('Connection to %s open', port);
-
     connection.on('data', function(data) {
         data = String(data).trim();
         
         if(data.indexOf("set") >= 0) {
             array_data = data.split(' ');
-            hash_array[array_data[1]] = array_data[2];
+            var key = array_data[1];
+            var value = array_data[2];
+
+            hash_array[key] = value;
             connection.write("STORED");
         } else if(data.indexOf("get") >= 0) {
             array_data = data.split(' ');
-            connection.write("VALUE " + hash_array[array_data[1]]);
+            var value = hash_array[array_data[1];
+
+            connection.write("VALUE " + value]);
         }
     });
 });
