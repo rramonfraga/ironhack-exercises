096543ae629f3b93474c3951c3f7bd79322a8aa8
LluisArevalo <thorin119@gmail.com>
2015-11-12 14:46:30 +0100

    Change variables name

--- Week3/javascript/simple_cached_client.js
+++ Week3/javascript/simple_cached_client.js
@@ -8,9 +8,9 @@ var options = {
 var client = new simplecached.Client(options, function(error){
   console.log("Connected to the server");
 
-  client.set("name", "Lluis", function(error, result){
-    client.get("name", function(error, result){
-      console.log(result);
+  client.set("name", "Lluis", function(error, response){
+    client.get("name", function(error, response){
+      console.log(response);
     });
   });
 
