e0c23a78e01ae2262bd0d19b227e63ebd65ffcd4
LluisArevalo <thorin119@gmail.com>
2015-11-16 15:47:33 +0100

    Add some functionalities to the Model

--- Week4/timetracking/app/models/project.rb
+++ Week4/timetracking/app/models/project.rb
@@ -6,4 +6,12 @@ class Project < ActiveRecord::Base
   def self.clean_old
     where('created_at < ?', Date.current - 1.week).destroy_all
   end
+
+  def self.last_created_projects(max)
+    order(created_at: :desc).limit(max)
+  end
+
+  def self.first_updated_projects(max)
+    order(updated_at: :asc).limit(max)
+  end
 end
