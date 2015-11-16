c61563108a87bcdfef6786ce01df658661491c27
LluisArevalo <thorin119@gmail.com>
2015-11-16 14:50:31 +0100

    Creating fake registers in the db through seeds.rd

--- Week4/timetracking/app/models/project.rb
+++ Week4/timetracking/app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   def self.iron_find(parameters)
     where('id = ?', parameters).first
   end
+
+  def self.clean_old
+    where('created_at < ?', Date.current - 1.week).destroy_all
+  end
 end
