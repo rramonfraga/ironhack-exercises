398aa6c711d04896973d681c6a113d80c52673d4
LluisArevalo <thorin119@gmail.com>
2015-11-18 15:34:04 +0100

    Show the errors of the form

--- Week4/timetracking/app/controllers/entries_controller.rb
+++ Week4/timetracking/app/controllers/entries_controller.rb
@@ -7,4 +7,26 @@ class EntriesController < ApplicationController
     @entries = @project.month_entries(date.year, date.month)
     @total_hours = @project.total_hours_in_month(date.year, date.month)
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @entry = @project.entries.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @entry = @project.entries.new(entry_params)
+
+    if(@entry.save)
+      redirect_to(action: 'index', controller: 'entries', project_id: @project.id)
+    else
+      render('new')
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:hours, :minutes, :date)
+  end
 end
--- Week4/timetracking/config/routes.rb
+++ Week4/timetracking/config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get "/" => 'site#home'
   get "/contact" => 'site#contact'
 
-  get "/projects" => 'projects#index'
-  get "/projects/:id" => 'projects#show'
-
-  get "/projects/:project_id/entries" => "entries#index"
+  resources :projects, only: [:index, :show] do
+    resources :entries, only: [:index, :new, :create]
+  end
 end
